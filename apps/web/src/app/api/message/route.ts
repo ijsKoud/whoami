f7a6d97bea71a4cfcc4d22c909406b96394b5689
Daan Klarenbeek <github@ijskoud.dev>
2023-10-15 20:51:08 +0200

    feat(Api): add message create route

---
+++ apps/web/src/app/api/message/route.ts
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { PrismaClient } from "@prisma/client";
+import { ZodError, z } from "zod";
+
+const prisma = new PrismaClient();
+const schema = z.object({
+	message: z.string({ required_error: "A message is required" }).max(1024, "A message cannot be longer than 1024 characters.")
+});
+
+export async function POST(request: NextRequest) {
+	const { getUser, isAuthenticated } = getKindeServerSession();
+	if (!isAuthenticated()) {
+		return new Response("Unauthorized", { status: 401 });
+	}
+
+	try {
+		const user = getUser();
+		const json = await request.json();
+		const { message } = schema.parse(json);
+
+		await prisma.message.create({
+			data: { content: message, name: `${user.given_name} ${user.family_name}`, userId: user.id!, picture: user.picture }
+		});
+	} catch (error) {
+		console.error(error);
+
+		if (error instanceof ZodError) {
+			const { message } = error.errors[0];
+			return NextResponse.json({ message }, { status: 400 });
+		}
+
+		return NextResponse.json({ message: "Unkown server error, please try again later." }, { status: 500 });
+	}
+
+	return NextResponse.json({ status: 200 });
+}
