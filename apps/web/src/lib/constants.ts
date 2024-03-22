3c75cb1b46cd812791809751ac8fd58ab5415974
Daan Klarenbeek <github@ijskoud.dev>
2024-03-22 17:39:46 +0100

    refactor: move constants and types to separate file

---
+++ apps/web/src/lib/constants.ts
@@ -0,0 +1,14 @@
+export const NAVIGATION_LINKS = [
+	{
+		name: "About",
+		href: "/#about"
+	},
+	{
+		name: "Projects",
+		href: "/#projects"
+	},
+	{
+		name: "Contact",
+		href: "/#contact"
+	}
+];
